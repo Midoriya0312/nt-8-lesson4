f96695d96e507781de57371f2f859918d14fb8c6
Midoriya0312 <oybektojiboyev0312@gmail.com>
2023-06-27 12:57:44 +0500

    lesson4

---
+++ main.js
@@ -0,0 +1,123 @@
+// let msg = 'hello students';
+
+// let strToArr = msg.split(' ');
+
+// for(let i = 0; i < strToArr.length; i++){
+//     let choped = strToArr[i].slice(1);
+//     let add = strToArr[i].charAt(0) +'ay';
+//     let res = choped + add;
+//     console.log(res);
+// }
+
+
+
+
+
+
+
+
+
+
+
+// Homework 'for'
+
+// 1 misol
+
+// function check(k, n) {
+//     for(let i = 0; i < n; i++) {
+//         console.log(k);
+//     }
+// }
+
+
+// check(2,3)
+
+
+// 2 misol
+
+// let a = 10, b = 20;
+
+// function check(a, b){
+//     if(a < b){
+//         for(let i = a; i > b; i++){
+//             console.log(a);
+//         }
+//     }
+
+// }
+
+
+// check(a,b)
+
+// 3 misol
+
+// let a = 1, b = 10;
+
+// function check(a, b){
+//     if(a < b){
+//         for(let i = b - 1; i > a; i--){
+//             console.log(i);
+//         }
+//     }
+// }
+
+// check(a,b)
+
+
+// 4 misol
+
+// let apple = [1,2,3,4,5,6,7,8,9,10];
+// function moneyApple(apple) {
+//     for (let i = 0; i < apple.lenght; i++) {
+//         let price = 1000;
+//         price *= apple[i];
+//         console.log(price);
+//     }
+// }
+
+// moneyApple(apple)
+
+
+
+// 5 misol
+
+// let confet = 10000;
+
+// for (let i = 0; i < 2; i += 0.2) {
+//     console.log(i*confet);
+// }
+
+
+
+
+
+
+
+
+// 9 misol
+
+
+// let a = 1;
+
+// let b = 4;
+// // 1 2 3 4
+// let daraja = a * a;
+
+// let yigindi = 0;
+
+// yigindi = yigindi +=
+
+
+
+
+
+
+// 13 misol
+
+// let n = 20;
+
+// for (let i = 3; i < n; i++) {
+//     if(i / i === 1 && i / 1 === i && i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
